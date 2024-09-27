b593090c139350f1084e22ec8156ed2ee67781f7
Nikk434 <nskshirsagar434@gmail.com>
2024-09-27 12:47:02 +0530

    dummy data

---
+++ restful-web-services/restful-web-services/src/main/resources/data.sql
@@ -0,0 +1,17 @@
+insert into user_details(id,birth_date,name)
+values(10321, current_date,'nik');
+
+INSERT INTO user_details(id, birth_date, name)
+VALUES (10322, current_date, 'alex');
+
+INSERT INTO user_details(id, birth_date, name)
+VALUES (10323, current_date, 'john');
+
+INSERT INTO user_details(id, birth_date, name)
+VALUES (10324, current_date, 'emma');
+
+INSERT INTO user_details(id, birth_date, name)
+VALUES (10325, current_date, 'sophia');
+
+INSERT INTO user_details(id, birth_date, name)
+VALUES (10326, current_date, 'liam');
