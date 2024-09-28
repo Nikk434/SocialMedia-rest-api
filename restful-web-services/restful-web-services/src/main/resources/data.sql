7562600fb042480a3a3a0f1098aa318692135360
Nikk434 <nskshirsagar434@gmail.com>
2024-09-28 12:50:39 +0530

    dummy data for post table

--- restful-web-services/restful-web-services/src/main/resources/data.sql
+++ restful-web-services/restful-web-services/src/main/resources/data.sql
@@ -15,3 +15,21 @@ VALUES (10325, current_date, 'sophia');
 
 INSERT INTO user_details(id, birth_date, name)
 VALUES (10326, current_date, 'liam');
+
+INSERT INTO post(id, user_id, description)
+VALUES(20001, 10321, 'learn AWS');
+
+INSERT INTO post(id, user_id, description)
+VALUES (20002, 10321, 'learning Spring Boot');
+
+INSERT INTO post(id, user_id, description)
+VALUES (20003, 10322, 'exploring Docker');
+
+INSERT INTO post(id, user_id, description)
+VALUES (20004, 10323, 'understanding microservices');
+
+INSERT INTO post(id, user_id, description)
+VALUES (20005, 10325, 'building REST APIs');
+
+INSERT INTO post(id, user_id, description)
+VALUES (20006, 10326, 'getting started with Kubernetes');
