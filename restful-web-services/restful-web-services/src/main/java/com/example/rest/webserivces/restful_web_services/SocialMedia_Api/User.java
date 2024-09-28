e8d015a5c1932f73ad4ad3c16c3add2c3ac19ab7
Nikk434 <nskshirsagar434@gmail.com>
2024-09-28 11:26:19 +0530

    For JPA to successfully create a new instance of an entity, the class must have a default constructor

--- restful-web-services/restful-web-services/src/main/java/com/example/rest/webserivces/restful_web_services/SocialMedia_Api/User.java
+++ restful-web-services/restful-web-services/src/main/java/com/example/rest/webserivces/restful_web_services/SocialMedia_Api/User.java
@@ -9,6 +9,10 @@ import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 @Entity(name = "user_details")
 public class User {
+
+    protected User(){
+
+    }
     @Id
     @GeneratedValue
     private Integer id;
