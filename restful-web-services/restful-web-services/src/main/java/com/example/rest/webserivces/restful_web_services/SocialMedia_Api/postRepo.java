06ec7032bf2afc7b17f91a8298136c097d6e243a
Nikk434 <nskshirsagar434@gmail.com>
2024-09-29 11:34:48 +0530

    to connect to post table in h2 database

---
+++ restful-web-services/restful-web-services/src/main/java/com/example/rest/webserivces/restful_web_services/SocialMedia_Api/postRepo.java
@@ -0,0 +1,8 @@
+package com.example.rest.webserivces.restful_web_services.SocialMedia_Api;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+// import org.springframework.data.jpa.repository.support.JpaRepositoryFactory;
+
+public interface postRepo extends JpaRepository <post, Integer>{
+    
+}
